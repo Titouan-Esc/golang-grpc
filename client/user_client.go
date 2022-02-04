018c7a076642a87697d18432699901bc072bd663
Titouan Escorneboueu <titouanescorneboueu@gmail.com>
2022-02-04 15:12:54 +0100

    user_client and everything is right

--- client/user_client.go
+++ client/user_client.go
@@ -1,1 +1,48 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/Titouan-Esc/golang-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// ? Connection au server
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	// ? Fermer la connection
+	defer conn.Close()
+
+	// ? Définir un nouveau client
+	client := pb.NewUserManagementClient(conn)
+
+	// ? Définir une nouveux context
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// ? Créer un map de users
+	var new_users = make(map[string]int32)
+	new_users["Gege"] = 62
+	new_users["Titouan"] = 21
+
+	// ? Créer un boucle pour créer les user avec le CreateNewuser
+	for name, age := range new_users {
+		res, err := client.CreateNewuser(ctx, &pb.NewUser{Name: name, Age: age})
+		if err != nil {
+			log.Fatalf("Could not create user: %v", err)
+		}
+		log.Printf(`User details:
+		Name: %s
+		Age: %d
+		Id: %d`, res.GetName(), res.GetAge(), res.GetId())
+	}
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/Titouan-Esc/golang-grpc
 
 go 1.17
 
-require google.golang.org/grpc v1.44.0
+require (
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+)
 
 require (
 	github.com/golang/protobuf v1.5.0 // indirect
@@ -10,6 +13,4 @@ require (
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 	golang.org/x/text v0.3.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 )
