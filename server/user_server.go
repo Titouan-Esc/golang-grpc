b998027eec7b402ac14f158f954e705ff51f06e4
Titouan Escorneboueu <titouanescorneboueu@gmail.com>
2022-02-07 21:44:19 +0100

    Add modification and func for GetUsers

--- server/user_server.go
+++ server/user_server.go
@@ -15,22 +15,34 @@ const (
 	port = ":50051"
 )
 
-// ? Implémentation du service grpc
-type UserManagementServer struct {
-	pb.UnimplementedUserManagementServer
+// ! Fonc pour la structure du UserManagementServer type
+func NewUserManagementServer() *UserManagementServer {
+	return &UserManagementServer{
+		user_list: &pb.UserList{},
+	}
 }
 
-// ? Méthode CreateNewuser
+// ! Implémentation du service grpc
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer // Ce connecte au server grpc
+
+	user_list *pb.UserList // Variable pour la liste des users
+}
+
+// ! Méthode CreateNewuser
 func (s *UserManagementServer) CreateNewuser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
 
 	// ? Création de l'id de l'user
 	var user_id int32 = int32(rand.Int31n(1000))
+	created_user := &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}
+	s.user_list.Users = append(s.user_list.Users, created_user)
 	// ? Retourner un user avec la référence du service protobuf
-	return &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}, nil
+	return created_user, nil
 }
 
-func main() {
+// ! Fonction qui vas run le code, ce qui nous permet de simplifier le code
+func (s *UserManagementServer) Run() error {
 	// ? Initialiser l'écoute au port
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -43,7 +55,21 @@ func main() {
 	pb.RegisterUserManagementServer(server, &UserManagementServer{})
 	log.Printf("Server listening at %v", lis.Addr())
 
-	if err := server.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+	return server.Serve(lis)
+}
+
+func (s *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UserList, error) {
+	return s.user_list, nil
+}
+
+func main() {
+	/*
+		C'est la seule chose que nous avons besoin d'avoir dans la func main
+	*/
+	// ? Instancier un nouveau UserManagementServer
+	var user_server *UserManagementServer = NewUserManagementServer()
+	// ? Appeler la fonction Run avec la variable au dessus 
+	if err := user_server.Run(); err != nil {
+		log.Fatalf("Failed to server: %v", err)
 	}
 }
